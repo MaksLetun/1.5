22d74518fc7309c5b1e014d82145d7d463d5be3e
MaksLetun <letunovskiy2012@yandex.ru>
2025-06-02 14:08:06 +0300

    1
    1

---
+++ script.js
@@ -0,0 +1,34 @@
+let cardList = document.querySelector('.card-list');
+let hideButton = document.querySelector('.action__button');
+let textSpan = document.querySelector('.action__text');
+let icon = document.querySelector('.action__icon');
+
+hideButton.addEventListener('click', function () {
+    if (textSpan.textContent === 'Показать все') {
+        cardList.classList.remove('visible');
+        textSpan.textContent = 'Скрыть';
+        icon.classList.add('rotate')
+    }
+
+    else {
+        cardList.classList.add('visible');
+        textSpan.textContent = 'Показать все';
+        icon.classList.remove('rotate')
+    }
+  });
+
+  
+  const swiper = new Swiper('.swiper', {
+    spaceBetween: -9,
+    slidesPerView: 'auto',
+    speed: 1000,
+    loop: false,
+    allowTouchMove: true,
+    autoHeight: true,
+    direction: 'horizontal',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+  });
